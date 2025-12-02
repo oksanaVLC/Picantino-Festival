ee53219c2d7559e1a95be6ce7bc84d04661e0fb0
Oksana Golubeva <oksana@uoc.edu>
2025-12-02 15:13:31 +0100

    Primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+// Import all of Bootstrap’s JS
+import * as bootstrap from "bootstrap";
+console.log(bootstrap);
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
