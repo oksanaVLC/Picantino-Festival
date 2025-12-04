f2f00f0457912f51578c014121997eb7c5ff5ba2
Oksana Golubeva <oksana@uoc.edu>
2025-12-04 15:11:28 +0100

    participants.html

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,12 +2,24 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
+import "../../../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
 // Import all of Bootstrap’s JS
 import * as bootstrap from "bootstrap";
 console.log(bootstrap);
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 /**
  * Write any other JavaScript below
  */
+
+const button = document.getElementById("load-more");
+button.addEventListener("click", () => {
+  document.querySelectorAll(".participant-card.extra").forEach((card) => {
+    card.style.display = "flex";
+  });
+  button.style.display = "none";
+});
